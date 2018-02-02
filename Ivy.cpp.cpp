8532065acd0e5573b1bcf3cc8f6fc1abf0b228f8
Ivy Khamisi <ivykendy@gmail.com>
2018-02-02 11:33:30 +0300

    added file

---
+++ Ivy.cpp.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+int addition(int x,int y) {int Z =x+y;
+return z;}
+int main()
+{
+int a;
+int b;
+cout<<"enter first number";
+cin>>a ;
+cout<<"enter second number";
+cin>>b;
+cout<<a;cout<<" + ";cout<<" = "; cout<< addition (a,b);
+    return 0;
+}
+
+
+
